92947202d01b29d6f78294cb0eedae48fbad3e4a
Cranival <111667804+Cranival@users.noreply.github.com>
2022-10-13 15:17:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -20,7 +20,7 @@ let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/ai_khanip",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/ai_khanip", 
+    description: "https://telegra.ph/file/c73f47b440241b66cb9d3.jpg", 
     title: 'Ƙαnnα 𝗕𝗢𝗧',
     body: wm,
     thumbnail: thumb,
